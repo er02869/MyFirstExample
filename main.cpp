f77deae81477a30267bde454acc2583caf6a2660
er02869 <waggysues@gmail.com>
2025-09-08 11:45:44 -0700

    Changed the operations output format

--- main.cpp
+++ main.cpp
@@ -9,16 +9,16 @@ int main()
 {
     cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+    int x, y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    cout << x << " + " << y << " = " << x + y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+    cout << x << " / " << y << " = " << x / y << endl;
+    cout << x << " / " << y << " = " << x / y << " with a remainder of " << x%y << endl;
+    cout << "Square Root of " << x << " is " << sqrt(x) << endl;
+    cout << "Square Root of " << y << " is " << sqrt(y) << endl;
 
     return 0;
 }
